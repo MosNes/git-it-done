5e0bf50057d1b934a63586bd70a2c4175595e369
MosNes <7hekarl@gmail.com>
2022-05-31 15:28:51 -0400

    tested API call function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,18 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+    
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //fetch returns a Promise
+    //.then executes whatever is in the parentheses when the Promise is fulfilled
+    fetch(apiUrl).then(function(response) {
+
+        //the .json() method returns another promise
+        //.then will execute whenever the promise is fulfilled
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+getUserRepos("facebook");
